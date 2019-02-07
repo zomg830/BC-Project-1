fda6809d50a367bba980e3186fe8a7702e9d4f90
zomg830 <matt.faubel@gmail.com>
2019-02-06 19:18:58 -0600

    added AuthenticJobs API

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
         console.log(locInput);
         $(".form-control").val("");
         searchDotGov(jobInput,locInput);
+        searchAuthenticJobs(jobInput,locInput);
     });
 
     var searchDotGov = (job, loc) => {
@@ -25,13 +26,15 @@ $(document).ready(function() {
         $.ajax({
             url: queryURL,
             method: "GET",
+            crossDomain: true,
+            dataType: "jsonp",
         }).then(function(response){
             console.log(response);
             for (let i = 0; i < response.length; i++){
-                var respObj = {
+                let respObj = {
                     title: response[i].position_title,
                     company: response[i].organization_name,
-                    location: response[i].locations[0],
+                    location: response[i].locations,
                     posted: response[i].start_date
                 }
                 aggregateResults.push(respObj);
@@ -39,6 +42,34 @@ $(document).ready(function() {
             console.log("Aggregate results: " + JSON.stringify(aggregateResults));
         })
     };
+
+    var searchAuthenticJobs = (job, loc) => {
+        var queryURL = `https://authenticjobs.com/api/?api_key=85265268d6f1738391d7c732415e84e9&method=aj.jobs.search&keywords=${job}&location=${loc}&format=json&perpage=10`
+        console.log("queryURL" + queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            crossDomain: true,
+            dataType: "jsonp",
+        }).then(function(response){
+            var ajResp = response.listings.listing;
+            console.log(ajResp);
+            for (let i = 0; i < ajResp.length; i++){
+                let respObj = {
+                    title: ajResp[i].category.name,
+                    company: ajResp[i].company.name,
+                    location: "",
+                    posted: ajResp[i].post_date
+                }
+                aggregateResults.push(respObj);
+            }
+            console.log("Aggregate results: " + JSON.stringify(aggregateResults));
+        })
+    }
+
+    var modalAppend = (arr) => {
+        $(".modal-body").append("<div>"+arr[0]+"</div>")
+    }
 });
 
 //modal search checkbox to select/un-select all job sites 
